3cabf76d22b88cf6e99da22fefa7b0735334e011
JIYONG JUNG <jeidiiy98@gmail.com>
2023-05-20 21:57:37 +0900

    build: react-md-editor 추가 및 css 오류 수정 (#32)

--- 003 Code/FE/notion-linked-blog/next.config.js
+++ 003 Code/FE/notion-linked-blog/next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const Dotenv = require("dotenv-webpack");
+const removeImports = require("next-remove-imports");
 
-const nextConfig = {
+const nextConfig = removeImports({
 	reactStrictMode: true,
 	webpack: config => {
 		config.plugins.push(new Dotenv({silent: true}));
 		return config;
 	},
-};
+});
 
 module.exports = nextConfig;
--- 003 Code/FE/notion-linked-blog/src/pages/_app.js
+++ 003 Code/FE/notion-linked-blog/src/pages/_app.js
@@ -1,11 +1,26 @@
 import {Provider} from "react-redux";
 
 import store from "@/redux/store";
-import "@/common/reset.css";
+import {createGlobalStyle} from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+	html,
+	body,
+	div#__next {
+		height: 100%;
+	}
+
+	html,
+	body {
+		margin: 0;
+		padding: 0;
+	}
+`;
 
 export default function App({Component, pageProps}) {
 	return (
 		<Provider store={store}>
+			<GlobalStyles />
 			<Component {...pageProps} />
 		</Provider>
 	);
