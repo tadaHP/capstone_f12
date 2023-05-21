5c0af79ca7e0e48c2cafa46b14bb576368546aac
JIYONG JUNG <jeidiiy98@gmail.com>
2023-05-21 14:35:14 +0900

    fix: Dotenv 설정이 제외되던 오류 해결 (#34)

--- 003 Code/FE/notion-linked-blog/next.config.js
+++ 003 Code/FE/notion-linked-blog/next.config.js
@@ -2,12 +2,12 @@
 const Dotenv = require("dotenv-webpack");
 const removeImports = require("next-remove-imports");
 
-const nextConfig = removeImports({
+const nextConfig = removeImports({});
+
+module.exports = nextConfig({
 	reactStrictMode: true,
 	webpack: config => {
 		config.plugins.push(new Dotenv({silent: true}));
 		return config;
 	},
 });
-
-module.exports = nextConfig;
