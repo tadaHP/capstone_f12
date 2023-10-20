d7e0eb2463b762c489e95680a6072de4714fd94e
tadaHP <gusvy0220@gmail.com>
2023-10-20 15:41:40 +0900

    feat: 배포를 위한 환경 분리 및 dockerfile작성 (#106)
    feat: redisConfig를 prod, dev 환경에 따라 분리
    feat: EmbeddedRedisConfig를 dev 환경에서만 작동 가능하게 변경
    feat: Dockerfile 작성

--- 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/common/config/EmbeddedRedisConfig.java
+++ 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/common/config/EmbeddedRedisConfig.java
@@ -10,11 +10,13 @@ import javax.annotation.PreDestroy;
 import org.apache.commons.lang3.SystemUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.util.StringUtils;
 
 import redis.embedded.RedisServer;
 
 @Configuration
+@Profile("dev")
 public class EmbeddedRedisConfig {
 	@Value("${spring.redis.port}")
 	private int port;
--- 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/common/config/RedisConfig.java
+++ 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/common/config/RedisConfig.java
@@ -3,42 +3,81 @@ package io.f12.notionlinkedblog.common.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 	@Value("${spring.redis.host}")
-	private String host;
+	private String embeddedRedisHost;
 
 	@Value("${spring.redis.port}")
-	private int port;
+	private int embeddedRedisPort;
 
+	@Value("${spring.data.redis.port}")
+	public int productionPort;
+	@Value("${spring.data.redis.host}")
+	public String productionHost;
+	@Value("${spring.data.redis.password}")
+	public String productionPassword;
+
+	//Prod
 	@Bean
-	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(host, port);
+	@Profile("prod")
+	public LettuceConnectionFactory productionRedisConnectionFactory() {
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+		redisStandaloneConfiguration.setHostName(productionHost);
+		redisStandaloneConfiguration.setPort(productionPort);
+		redisStandaloneConfiguration.setPassword(productionPassword);
+		return new LettuceConnectionFactory(redisStandaloneConfiguration);
 	}
 
 	@Bean
-	public RedisTemplate<String, Object> redisTemplate() {
+	public RedisTemplate<String, Object> productionRedisTemplate(RedisConnectionFactory connectionFactory) {
 		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
-		redisTemplate.setValueSerializer(new StringRedisSerializer());
-		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+		redisTemplate.setConnectionFactory(connectionFactory);
 		return redisTemplate;
 	}
 
 	@Bean
-	public StringRedisTemplate stringRedisTemplate() {
-		StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
-
-		stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
-		stringRedisTemplate.setValueSerializer(new StringRedisSerializer());
-		stringRedisTemplate.setConnectionFactory(redisConnectionFactory());
-
-		return stringRedisTemplate;
+	StringRedisTemplate productionStringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		StringRedisTemplate template = new StringRedisTemplate();
+		template.setConnectionFactory(redisConnectionFactory);
+		return template;
 	}
+
+	//Embedded
+	@Bean
+	@Profile("dev")
+	public RedisConnectionFactory embeddedRedisConnectionFactory() {
+		return new LettuceConnectionFactory(embeddedRedisHost, embeddedRedisPort);
+	}
+
+	// @Bean
+	// public RedisTemplate<String, Object> embeddedRedisTemplate() {
+	// 	RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+	// 	redisTemplate.setKeySerializer(new StringRedisSerializer());
+	// 	redisTemplate.setValueSerializer(new StringRedisSerializer());
+	// 	redisTemplate.setConnectionFactory(redisConnectionFactory());
+	// 	return redisTemplate;
+	// }
+	//
+	// @Bean
+	// public StringRedisTemplate embeddedStringRedisTemplate() {
+	// 	StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+	//
+	// 	stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
+	// 	stringRedisTemplate.setValueSerializer(new StringRedisSerializer());
+	// 	stringRedisTemplate.setConnectionFactory(redisConnectionFactory());
+	//
+	// 	return stringRedisTemplate;
+	// }
 }
